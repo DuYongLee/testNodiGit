8647d2e9f470c313dda81da1d4124ac55e196d6a
DuYongLee <konan5847@gmail.com>
2016-08-24 15:09:02 +0900

    test

--- gitTest.js
+++ gitTest.js
@@ -5,56 +5,53 @@ const fs = require("fs");
 const app = yaml.parse(fs.readFileSync('./app.yaml', 'utf8'));
 const appPartner = yaml.parse(fs.readFileSync('./appPartner.yaml', 'utf8'));
 
-const pharses =  ['major', 'minor', 'patch'];
-const reg = /^v(\d{1,})-(\d{1,})-(\d{1,})/;
-const version = app.version;
+const date = new Date();
+const Year = (date.getFullYear()).toString().slice(2);
+const Month = ("00" + (date.getMonth()+1)).slice(1);
+const Day = date.getDate();
+const Hours = date.getHours();
+const Minutes = date.getMinutes();
 
-const pharse = 'patch';
-const pharseIndex = pharses.indexOf(pharse);
-const changedNum = (+version.match(reg)[pharseIndex + 1] + 1).toString();
+const version = `v${Year}.${Month}.${Day}.${Hours}.${Minutes}`;
 
-let changedIdx;
-const pattern = pharses.map(function(value, idx) {
-  if (value === pharse) {
-    changedIdx = idx;
-    return changedNum;
-  } else if(changedIdx < idx) {
-    return '0';
-  } else {
-    return '$' + (++idx);
-  }
-});
+app.version = version;
+appPartner.version = version;
 
-app.version = version.replace(reg, 'v' + pattern.join('-'));
-appPartner.version = version.replace(reg, 'v' + pattern.join('-'));
+let currentBranchName = "";
 
 fs.writeFileSync('./app.yaml', yaml.stringify(app, 4, 2));
+fs.writeFileSync('./appPartner.yaml', yaml.stringify(appPartner, 4, 2));
 
 Git.Repository.open('./').then(repo => {
-  repo.getHeadCommit().then(commit => {
-    return repo.createTag(commit, app.version, 'version up tag').then(tag => {
+  return repo.createCommitOnHead(['gitTest.js'], Git.Signature.default(repo), Git.Signature.default(repo)).then(oid => {
+    repo.getCommit(oid);
+  }).then(commit => {
+    repo.getCurrentBranch().then(reference => {
+      currentBranchName = reference.name();
+    })
+    repo.createTag(commit, version, 'version up tag').then(tag => {
       console.log(tag.name());
     });
   }).then(() => {
     return repo.getRemote("origin");
-  }).then(function(remote) {
-    return remote.push(
-      ["refs/heads/master:refs/heads/master"],
-      {
-        callbacks: {
-          certificateCheck: function() { return 1; },
-          credentials: function(url, userName) {
-            console.log(url);
-            console.log(userName);
-            return Git.Cred.sshKeyFromAgent(userName);
-          }
-        }
-      }
-    );
-  }).then(function() {
-      console.log('remote Pushed!')
-  })
-  .catch(function(reason) {
+  // }).then(remote => {
+    // return remote.push(
+    //   [
+    //   `${currentBranchName}:${currentBranchName}`,
+    //   `refs/tags/${app.version}:refs/tags/${app.version}`
+    //   ],
+    //   {
+    //     callbacks: {
+    //       certificateCheck: () => { return 1; },
+    //       credentials: (url, userName) => {
+    //         return Git.Cred.sshKeyFromAgent(userName);
+    //       }
+    //     }
+    //   }
+    // );
+  }).then(() => {
+      console.log('remote Pushed!');
+  }).catch(reason => {
       console.log(reason);
   })
 })
